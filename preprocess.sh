0135a32a64eed5f88795cf5cedbbf2f09fbeb504
yil8 <yil8@uci.edu>
2015-06-28 16:00:01 -0700

    On branch master preprocess.sh added

---
+++ preprocess.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+gct2npy.py bgedv2_QNORM.gctx bgedv2_float64.npy
+
+
+kmeans.py
+nodup_idx.py
+
+
+bgedv2.py
+bgedv2_reqnorm.py
+bgedv2_norm.py
+
+
+GTEx.py
+GTEx_reqnorm.py
+GTEx_norm.py
+
+
+1000G.py
+1000G_reqnorm.py
+1000G_norm.py
+
